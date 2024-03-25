f9296b27aa75521c5a0e156d84517be5e5c795ce
YAN Wenkun <code@yanwk.fun>
2024-03-26 06:49:15 +0800

    Rehaul step1

--- step1.sh
+++ step1.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
+set -eu
 
 git clone --depth=1 --no-tags --recurse-submodules --shallow-submodules \
     https://github.com/MrForExample/ComfyUI-3D-Pack.git
@@ -7,6 +8,7 @@ python -m pip wheel --no-cache-dir \
     ./ComfyUI-3D-Pack/_Pre_Builds/_Wheels_win_py311_cu121/*.whl \
         -w ./temp_wheel_dir
 
+# Put everything together to resolve compatible versions
 python -m pip wheel --no-cache-dir \
     xformers torchvision torchaudio \
     -r requirements.txt \
@@ -16,8 +18,8 @@ python -m pip wheel --no-cache-dir \
         --extra-index-url https://pypi.org/simple \
         -w ./temp_wheel_dir
 
-mv temp_wheel_dir cu121_python_deps
+mv temp_wheel_dir cp311_cu121_deps
 
-tar cf cu121_python_deps.tar cu121_python_deps
+tar cf cp311_cu121_deps.tar cp311_cu121_deps
 
-ls -lahF cu121_python_deps
+ls -lahF cp311_cu121_deps
