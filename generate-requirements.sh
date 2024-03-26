b577f278292a3c0a9079d4c85f8730a69d0fcb7c
YAN Wenkun <code@yanwk.fun>
2024-03-26 08:45:17 +0800

    fine-tune deps

--- generate-requirements.sh
+++ generate-requirements.sh
@@ -44,7 +44,7 @@ curl -w "\n" https://raw.githubusercontent.com/ZHO-ZHO-ZHO/ComfyUI-InstantID/mai
 sed -i '/^#/d' requirements.txt
 sed -i 's/[[:space:]]*$//' requirements.txt
 
-sort -bdfu requirements.txt > tmpfile && mv tmpfile requirements.txt
+sort -u requirements.txt > tmpfile && mv tmpfile requirements.txt
 
 
 echo "<requirements.txt> generated. Check before use."
