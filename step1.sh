81c397ecb11a2f895312e331d7ae0f74b6f4e868
YAN Wenkun <code@yanwk.fun>
2024-03-26 10:50:38 +0800

    Rehaul step1

--- step1.sh
+++ step1.sh
@@ -1,27 +1,59 @@
 #!/bin/bash
 set -eux
 
-git clone --depth=1 --no-tags --recurse-submodules --shallow-submodules \
-    https://github.com/MrForExample/ComfyUI-3D-Pack.git
+gcs='git clone --depth=1 --no-tags --recurse-submodules --shallow-submodules'
 
-python -m pip wheel --no-cache-dir \
-    ./ComfyUI-3D-Pack/_Pre_Builds/_Wheels_win_py311_cu121/*.whl \
-        -w ./temp_wheel_dir
+workdir=$(pwd)
 
-# Put everything together to resolve compatible versions
-python -m pip wheel --no-cache-dir \
+# Setup Python embeded
+
+curl https://www.python.org/ftp/python/3.11.8/python-3.11.8-embed-amd64.zip \
+    -o python_embeded.zip
+unzip python_embeded.zip -d "$workdir"/python_embeded
+
+cd "$workdir"/python_embeded
+echo 'import site' >> ./python311._pth
+curl https://bootstrap.pypa.io/get-pip.py -o get-pip.py
+./python.exe get-pip.py
+
+# PIP install
+
+./python.exe -s -m pip install \
     xformers torchvision torchaudio \
-    -r requirements.txt \
-    onnxruntime-gpu \
-        --extra-index-url https://download.pytorch.org/whl/cu121 \
-        --extra-index-url https://aiinfra.pkgs.visualstudio.com/PublicPackages/_packaging/onnxruntime-cuda-12/pypi/simple/ \
-        -w ./temp_wheel_dir
+    --index-url https://download.pytorch.org/whl/cu121 \
+    --extra-index-url https://pypi.org/simple
 
-# Test-install to check conflicts
-python -m pip install --no-cache-dir ./temp_wheel_dir/*
+./python.exe -s -m pip install \
+    -r requirements.txt
 
-mv temp_wheel_dir cp311_cu121_deps
+./python.exe -s -m pip install \
+    --force-reinstall onnxruntime-gpu \
+    --index-url https://aiinfra.pkgs.visualstudio.com/PublicPackages/_packaging/onnxruntime-cuda-12/pypi/simple/ \
+    --extra-index-url https://pypi.org/simple \
+&& ./python.exe -s -m pip install \
+    mediapipe
 
-tar cf cp311_cu121_deps.tar cp311_cu121_deps
+# Deps for ComfyUI-3D-Pack
 
-ls -lahF cp311_cu121_deps
+$gcs https://github.com/MrForExample/ComfyUI-3D-Pack.git \
+    "$workdir"/ComfyUI-3D-Pack
+
+./python.exe -s -m pip install \
+    "$workdir"/ComfyUI-3D-Pack/_Pre_Builds/_Wheels_win_py311_cu121/*.whl
+
+cp -rf \
+    "$workdir"/ComfyUI-3D-Pack/_Pre_Builds/_Python311_cpp/include \
+    "$workdir"/python_embeded/include
+
+cp -rf \
+    "$workdir"/ComfyUI-3D-Pack/_Pre_Builds/_Python311_cpp/libs \
+    "$workdir"/python_embeded/libs
+
+rm -rf "$workdir"/ComfyUI-3D-Pack
+
+# Finishing
+sed -i '1i../ComfyUI' ./python311._pth
+
+cd "$workdir"
+
+ls -lahF "$workdir"
