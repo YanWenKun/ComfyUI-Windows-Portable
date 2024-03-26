5dbe9c446927ec128818f350e4c1eadde565cbdf
YAN Wenkun <code@yanwk.fun>
2024-03-26 09:42:48 +0800

    change priority of pypi

--- step1.sh
+++ step1.sh
@@ -13,9 +13,8 @@ python -m pip wheel --no-cache-dir \
     xformers torchvision torchaudio \
     -r requirements.txt \
     onnxruntime-gpu \
-        --index-url https://download.pytorch.org/whl/cu121 \
+        --extra-index-url https://download.pytorch.org/whl/cu121 \
         --extra-index-url https://aiinfra.pkgs.visualstudio.com/PublicPackages/_packaging/onnxruntime-cuda-12/pypi/simple/ \
-        --extra-index-url https://pypi.org/simple \
         -w ./temp_wheel_dir
 
 # Test-install to check conflicts
