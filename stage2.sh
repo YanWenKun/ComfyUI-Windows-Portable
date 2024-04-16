43022af9fad57d06863636b146a669c7508e76aa
YAN Wenkun <code@yanwk.fun>
2024-04-16 18:35:10 +0800

    re-arrange nodes

--- stage2.sh
+++ stage2.sh
@@ -21,53 +21,71 @@ cp taesd/*.pth \
     "$workdir"/ComfyUI_Windows_portable/ComfyUI/models/vae_approx/
 rm -rf taesd
 
+# CUSTOM NODES
 cd "$workdir"/ComfyUI_Windows_portable/ComfyUI/custom_nodes
+$gcs https://github.com/ltdrdata/ComfyUI-Manager.git
+
+# Workspace
 $gcs https://github.com/AIGODLIKE/AIGODLIKE-ComfyUI-Translation.git
-$gcs https://github.com/bash-j/mikey_nodes.git
-$gcs https://github.com/chrisgoringe/cg-use-everywhere.git
 $gcs https://github.com/crystian/ComfyUI-Crystools.git
 $gcs https://github.com/crystian/ComfyUI-Crystools-save.git
+
+# General
+$gcs https://github.com/bash-j/mikey_nodes.git
+$gcs https://github.com/chrisgoringe/cg-use-everywhere.git
 $gcs https://github.com/cubiq/ComfyUI_essentials.git
-$gcs https://github.com/cubiq/ComfyUI_FaceAnalysis.git
+$gcs https://github.com/Derfuu/Derfuu_ComfyUI_ModdedNodes.git
+$gcs https://github.com/jags111/efficiency-nodes-comfyui.git
+$gcs https://github.com/kijai/ComfyUI-KJNodes.git
+$gcs https://github.com/pythongosssss/ComfyUI-Custom-Scripts.git
+$gcs https://github.com/rgthree/rgthree-comfy.git
+$gcs https://github.com/shiimizu/ComfyUI_smZNodes.git
+$gcs https://github.com/Suzie1/ComfyUI_Comfyroll_CustomNodes.git
+
+# Control
 $gcs https://github.com/cubiq/ComfyUI_InstantID.git
 $gcs https://github.com/cubiq/ComfyUI_IPAdapter_plus.git
-$gcs https://github.com/Derfuu/Derfuu_ComfyUI_ModdedNodes.git
 $gcs https://github.com/Fannovel16/comfyui_controlnet_aux.git
-$gcs https://github.com/Fannovel16/ComfyUI-Frame-Interpolation.git
-$gcs https://github.com/FizzleDorf/ComfyUI_FizzNodes.git
 $gcs https://github.com/florestefano1975/comfyui-portrait-master.git
 $gcs https://github.com/Gourieff/comfyui-reactor-node.git
 $gcs https://github.com/huchenlei/ComfyUI-layerdiffuse.git
-$gcs https://github.com/jags111/efficiency-nodes-comfyui.git
-$gcs https://github.com/kijai/ComfyUI-KJNodes.git
 $gcs https://github.com/Kosinkadink/ComfyUI-Advanced-ControlNet.git
-$gcs https://github.com/Kosinkadink/ComfyUI-AnimateDiff-Evolved.git
-$gcs https://github.com/Kosinkadink/ComfyUI-VideoHelperSuite.git
 $gcs https://github.com/ltdrdata/ComfyUI-Impact-Pack.git
 $gcs https://github.com/ltdrdata/ComfyUI-Inspire-Pack.git
-$gcs https://github.com/ltdrdata/ComfyUI-Manager.git
 $gcs https://github.com/mcmonkeyprojects/sd-dynamic-thresholding.git
+$gcs https://github.com/storyicon/comfyui_segment_anything.git
+$gcs https://github.com/twri/sdxl_prompt_styler.git
+
+# Video
+$gcs https://github.com/Fannovel16/ComfyUI-Frame-Interpolation.git
+$gcs https://github.com/FizzleDorf/ComfyUI_FizzNodes.git
+$gcs https://github.com/Kosinkadink/ComfyUI-AnimateDiff-Evolved.git
+$gcs https://github.com/Kosinkadink/ComfyUI-VideoHelperSuite.git
 $gcs https://github.com/melMass/comfy_mtb.git
-$gcs https://github.com/MrForExample/ComfyUI-3D-Pack.git
 $gcs https://github.com/MrForExample/ComfyUI-AnimateAnyone-Evolved.git
-$gcs https://github.com/pythongosssss/ComfyUI-Custom-Scripts.git
+
+# More
+$gcs https://github.com/cubiq/ComfyUI_FaceAnalysis.git
+$gcs https://github.com/MrForExample/ComfyUI-3D-Pack.git
 $gcs https://github.com/pythongosssss/ComfyUI-WD14-Tagger.git
-$gcs https://github.com/rgthree/rgthree-comfy.git
-$gcs https://github.com/shiimizu/ComfyUI_smZNodes.git
 $gcs https://github.com/SLAPaper/ComfyUI-Image-Selector.git
 $gcs https://github.com/ssitu/ComfyUI_UltimateSDUpscale.git
-$gcs https://github.com/storyicon/comfyui_segment_anything.git
-$gcs https://github.com/Suzie1/ComfyUI_Comfyroll_CustomNodes.git
-$gcs https://github.com/twri/sdxl_prompt_styler.git
 $gcs https://github.com/WASasquatch/was-node-suite-comfyui.git
 
+
 cd "$workdir"
-mv python_embeded ComfyUI_Windows_portable
+mv  python_embeded  ComfyUI_Windows_portable/python_embeded
 
 cd "$workdir"/ComfyUI_Windows_portable
 mkdir update
 cp -r ComfyUI/.ci/update_windows/* ./update/
 cp -r ComfyUI/.ci/windows_base_files/* ./
+
+# Setup Python embeded, part 3/3
+sed -i '1irem .\\python_embeded\\python.exe -s ComfyUI\\main.py --windows-standalone-build --disable-auto-launch' ./run_nvidia_gpu.bat
+sed -i '1irem set PYTHONPYCACHEPREFIX=.\\pycache' ./run_nvidia_gpu.bat
+sed -i '1irem set HTTPS_PROXY=http://localhost:1081' ./run_nvidia_gpu.bat
+sed -i '1irem set HTTP_PROXY=http://localhost:1081' ./run_nvidia_gpu.bat
 
 du -hd1 "$workdir"
 
