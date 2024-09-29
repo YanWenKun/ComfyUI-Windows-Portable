72227290c07da2202fd6a7b864f4d0fb729e9dc6
YAN Wenkun <code@yanwk.fun>
2024-09-29 20:35:58 +0800

    Add PIP & HF Hub mirror settings in startup script

--- attachments/run_nvidia_gpu.bat
+++ attachments/run_nvidia_gpu.bat
@@ -3,11 +3,14 @@
 rem set HTTP_PROXY=http://localhost:1081
 rem set HTTPS_PROXY=http://localhost:1081
 
-@REM In order to save your time on compiling PyTorch JIT CPP extensions,
-@REM  edit this according to your GPU arch.
-@REM https://github.com/ashawkey/stable-dreamfusion/issues/360#issuecomment-2292510049
+@REM To set mirror site for PIP & HuggingFace Hub, uncomment and edit the two lines below.
+@REM 如需配置 PIP 与 HuggingFace Hub 镜像，取消注释并编辑下两行。
+rem set PIP_INDEX_URL="https://mirrors.tuna.tsinghua.edu.cn/pypi/web/simple"
+rem set HF_ENDPOINT="https://hf-mirror.com"
+
+@REM In order to save your time on compiling PyTorch JIT CPP extensions, edit this line according to your GPU arch.
 @REM 依照下表，修改为你的 GPU 对应架构，以节约 JIT 编译 PyTorch C++ 扩展的时间：
-@REM https://github.com/ashawkey/stable-dreamfusion/issues/360#issuecomment-2292510049
+@REM Ref: https://github.com/ashawkey/stable-dreamfusion/issues/360#issuecomment-2292510049
 set TORCH_CUDA_ARCH_LIST=5.2+PTX;6.0;6.1+PTX;7.5;8.0;8.6;8.9+PTX
 
 @REM This command will set PATH environment variable.
@@ -18,8 +21,7 @@ set PATH=%PATH%;%~dp0\python_embeded\Scripts
 @REM 该行命令会使 .pyc 文件集中保存在一处。
 set PYTHONPYCACHEPREFIX=.\pycache
 
-@REM If you don't want the browser to open automatically,
-@REM  add " --disable-auto-launch" (without quotation marks) to the end of the line below.
+@REM If you don't want the browser to open automatically, add " --disable-auto-launch" (without quotation marks) to the end of the line below.
 @REM 如果不想要 ComfyUI 启动后自动打开浏览器，添加" --disable-auto-launch"（不含引号）到下行末尾。
 .\python_embeded\python.exe -s ComfyUI\main.py --windows-standalone-build
 
