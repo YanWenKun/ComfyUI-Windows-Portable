d110937d762873696b281d28e95a798a3fabcd76
YAN Wenkun <code@yanwk.fun>
2024-10-27 08:59:11 +0800

    Upgrade to pt25+cu124

--- stage1.sh
+++ stage1.sh
@@ -16,33 +16,22 @@ curl https://www.python.org/ftp/python/3.12.7/python-3.12.7-embed-amd64.zip \
     -o python_embeded.zip
 unzip python_embeded.zip -d "$workdir"/python_embeded
 
-# ComfyUI-3D-Pack, part 1/2
-$gcs https://github.com/MrForExample/Comfy3D_Pre_Builds.git \
-    "$workdir"/Comfy3D_Pre_Builds
-
-mv \
-    "$workdir"/Comfy3D_Pre_Builds/_Python_Source_cpp/py312/include \
-    "$workdir"/python_embeded/include
-
-mv \
-    "$workdir"/Comfy3D_Pre_Builds/_Python_Source_cpp/py312/libs \
-    "$workdir"/python_embeded/libs
-
 # Setup Python embeded, part 2/3
 cd "$workdir"/python_embeded
 echo 'import site' >> ./python312._pth
 curl https://bootstrap.pypa.io/get-pip.py -o get-pip.py
 ./python.exe get-pip.py
 
-# PIP install
+# PIP install PyTorch
 ./python.exe -s -m pip install \
     --upgrade pip wheel setuptools Cython cmake
 
 ./python.exe -s -m pip install \
-    xformers==0.0.27.post2 torchvision==0.19.0 torchaudio \
-    --index-url https://download.pytorch.org/whl/cu121 \
+    xformers==0.0.28.post2 torch==2.5.0 torchvision torchaudio \
+    --index-url https://download.pytorch.org/whl/cu124 \
     --extra-index-url https://pypi.org/simple
 
+# PIP install
 # 1. requirements.txt
 # 2. onnxruntime-gpu
 # 3. requirements2.txt
@@ -59,19 +48,33 @@ curl https://bootstrap.pypa.io/get-pip.py -o get-pip.py
 ./python.exe -s -m pip install \
     -r "$workdir"/requirements2.txt
 
-# Fix broken dep for mediapipe
-./python.exe -s -m pip install \
-    mediapipe
+# ComfyUI-3D-Pack, part 1/2
+$gcs https://github.com/MrForExample/Comfy3D_Pre_Builds.git \
+    "$workdir"/Comfy3D_Pre_Builds
+
+mv \
+    "$workdir"/Comfy3D_Pre_Builds/_Python_Source_cpp/py312/include \
+    "$workdir"/python_embeded/include
+
+mv \
+    "$workdir"/Comfy3D_Pre_Builds/_Python_Source_cpp/py312/libs \
+    "$workdir"/python_embeded/libs
+
+rm -rf "$workdir"/Comfy3D_Pre_Builds
 
 # ComfyUI-3D-Pack, part 2/2
 ./python.exe -s -m pip install \
-    "$workdir"/Comfy3D_Pre_Builds/_Build_Wheels/_Wheels_win_py312_torch2.4.0_cu121/*.whl
+https://github.com/YanWenKun/ComfyUI-Windows-Portable/releases/download/v5.3-wheels/pointnet2_ops-3.0.0-cp312-cp312-win_amd64.whl \
+https://github.com/YanWenKun/ComfyUI-Windows-Portable/releases/download/v5.3-wheels/simple_knn-0.0.0-cp312-cp312-win_amd64.whl \
+https://github.com/YanWenKun/ComfyUI-Windows-Portable/releases/download/v5.3-wheels/diff_gaussian_rasterization-0.0.0-cp312-cp312-win_amd64.whl \
+https://github.com/YanWenKun/ComfyUI-Windows-Portable/releases/download/v5.3-wheels/kiui-0.2.14-py3-none-any.whl \
+https://github.com/YanWenKun/ComfyUI-Windows-Portable/releases/download/v5.3-wheels/nvdiffrast-0.3.3-py3-none-any.whl \
+https://github.com/YanWenKun/ComfyUI-Windows-Portable/releases/download/v5.3-wheels/torch_scatter-2.1.2-cp312-cp312-win_amd64.whl \
+https://github.com/YanWenKun/ComfyUI-Windows-Portable/releases/download/v5.3-wheels/pytorch3d-0.7.8-cp312-cp312-win_amd64.whl
 
-# From: https://github.com/rusty1s/pytorch_scatter?tab=readme-ov-file#binaries
+# Fix broken dep for mediapipe
 ./python.exe -s -m pip install \
-    torch-scatter -f https://data.pyg.org/whl/torch-2.4.0%2Bcu121.html
-
-rm -rf "$workdir"/Comfy3D_Pre_Builds
+    mediapipe
 
 # Add Ninja binary (replacing PIP one)
 curl -L https://github.com/ninja-build/ninja/releases/latest/download/ninja-win.zip \
