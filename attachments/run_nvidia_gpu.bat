de441ec07437c06b05ea519628fa8a0cb590a941
YAN Wenkun <code@yanwk.fun>
2024-10-29 05:30:59 +0800

    Add script files

--- attachments/run_nvidia_gpu.bat
+++ attachments/run_nvidia_gpu.bat
@@ -1,28 +1,22 @@
 @REM To set proxy, edit and uncomment the two lines below (remove 'rem ' in the beginning of line).
-@REM 如需配置代理，编辑下两行命令，并取消注释（移除行首的 'rem '）。
 rem set HTTP_PROXY=http://localhost:1081
 rem set HTTPS_PROXY=http://localhost:1081
 
 @REM To set mirror site for PIP & HuggingFace Hub, uncomment and edit the two lines below.
-@REM 如需配置 PIP 与 HuggingFace Hub 镜像，取消注释并编辑下两行。
 rem set PIP_INDEX_URL=https://mirrors.tuna.tsinghua.edu.cn/pypi/web/simple
 rem set HF_ENDPOINT=https://hf-mirror.com
 
 @REM In order to save your time on compiling PyTorch JIT CPP extensions, edit this line according to your GPU arch.
-@REM 依照下表，修改为你的 GPU 对应架构，以节约 JIT 编译 PyTorch C++ 扩展的时间：
 @REM Ref: https://github.com/ashawkey/stable-dreamfusion/issues/360#issuecomment-2292510049
 set TORCH_CUDA_ARCH_LIST=5.2+PTX;6.0;6.1+PTX;7.5;8.0;8.6;8.9+PTX
 
 @REM This command will set PATH environment variable.
-@REM 该行命令会配置 PATH 环境变量。
 set PATH=%PATH%;%~dp0\python_embeded\Scripts
 
 @REM This command will let the .pyc files to be stored in one place.
-@REM 该行命令会使 .pyc 文件集中保存在一处。
 set PYTHONPYCACHEPREFIX=%~dp0\pycache
 
 @REM This command will copy u2net.onnx to user's home path, to skip download at first start.
-@REM 该行命令会复制 u2net.onnx 到用户主目录下，以免启动时还需下载。
 IF NOT EXIST "%USERPROFILE%\.u2net\u2net.onnx" (
     IF EXIST ".\extras\u2net.onnx" (
         mkdir "%USERPROFILE%\.u2net" 2>nul
@@ -31,7 +25,6 @@ IF NOT EXIST "%USERPROFILE%\.u2net\u2net.onnx" (
 )
 
 @REM If you don't want the browser to open automatically, add " --disable-auto-launch" (without quotation marks) to the end of the line below.
-@REM 如果不想要 ComfyUI 启动后自动打开浏览器，添加" --disable-auto-launch"（不含引号）到下行末尾。
 .\python_embeded\python.exe -s ComfyUI\main.py --windows-standalone-build
 
 pause
---
+++ attachments/中文脚本/run_cn.bat
@@ -0,0 +1,38 @@
+@REM 如需配置代理，编辑下两行命令，并取消注释（移除行首的 'rem '）。
+rem set HTTP_PROXY=http://localhost:1081
+rem set HTTPS_PROXY=http://localhost:1081
+
+@REM 如需配置 PIP 与 HuggingFace Hub 镜像，编辑下两行。
+set PIP_INDEX_URL=https://mirrors.tuna.tsinghua.edu.cn/pypi/web/simple
+set HF_ENDPOINT=https://hf-mirror.com
+
+@REM 依照下表，修改为你的 GPU 对应架构，以节约 JIT 编译 PyTorch C++ 扩展的时间：
+@REM 'Pascal', '6.0;6.1+PTX'
+@REM 'Volta+Tegra', '7.2'
+@REM 'Volta', '7.0+PTX'
+@REM 'Turing', '7.5+PTX'
+@REM 'Ampere+Tegra', '8.7'
+@REM 'Ampere', '8.0;8.6+PTX'
+@REM 'Ada', '8.9+PTX'
+@REM 'Hopper', '9.0+PTX'
+set TORCH_CUDA_ARCH_LIST=5.2+PTX;6.0;6.1+PTX;7.5;8.0;8.6;8.9+PTX
+
+@REM 该行命令会配置 PATH 环境变量。
+set PATH=%PATH%;%~dp0\python_embeded\Scripts
+
+@REM 该行命令会使 .pyc 文件集中保存在一处。
+set PYTHONPYCACHEPREFIX=%~dp0\pycache
+
+@REM 该命令会复制 u2net.onnx 到用户主目录下，以免启动时还需下载。
+IF NOT EXIST "%USERPROFILE%\.u2net\u2net.onnx" (
+    IF EXIST ".\extras\u2net.onnx" (
+        mkdir "%USERPROFILE%\.u2net" 2>nul
+        copy ".\extras\u2net.onnx" "%USERPROFILE%\.u2net\u2net.onnx"
+    )
+)
+
+@REM 如果不想要 ComfyUI 启动后自动打开浏览器，添加" --disable-auto-launch"（不含引号）到下行末尾。
+@REM 如果用 40 系显卡，可添加" --fast"
+.\python_embeded\python.exe -s ComfyUI\main.py --windows-standalone-build
+
+pause
