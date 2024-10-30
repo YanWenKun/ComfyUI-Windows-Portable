4ebf8c7642df34ca9d823a63e938a61dfca6dba8
YAN Wenkun <code@yanwk.fun>
2024-10-31 05:31:53 +0800

    Adjust compression level
    To compare the file size

--- stage3.sh
+++ stage3.sh
@@ -7,6 +7,7 @@ du -hd1
 
 # LZMA2 is ~75% faster than LZMA, but consumes significant more RAM
 # Use 2140000000b as volume size just because GitHub think 2147483648b is "larger than 2GB"
-"C:\Program Files\7-Zip\7z.exe" a -t7z -m0=lzma2 -mx=7 -mfb=64 -md=32m -ms=on -mf=BCJ2 -v2140000000b ComfyUI_Windows_portable_cu124.7z ComfyUI_Windows_portable
+# The param "-mx=5 -mfb=32 -md=16m" is equivalent to "Normal Compression" in 7Zip GUI
+"C:\Program Files\7-Zip\7z.exe" a -t7z -m0=lzma2 -mx=5 -mfb=32 -md=16m -ms=on -mf=BCJ2 -v2140000000b ComfyUI_Windows_portable_cu124.7z ComfyUI_Windows_portable
 
 ls -lahF
