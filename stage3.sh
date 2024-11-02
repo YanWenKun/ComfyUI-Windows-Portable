1eb923373e6d9f819cc1611151b418e4bc1cce2d
YAN Wenkun <code@yanwk.fun>
2024-11-02 11:24:21 +0800

    Testing "Quick Compression" for 7z

--- stage3.sh
+++ stage3.sh
@@ -27,6 +27,6 @@ du -hd1
 # The file size difference is small.
 # So I choose the "Normal Compression", not for faster compression time, but for faster decompression time.
 
-"C:\Program Files\7-Zip\7z.exe" a -t7z -m0=lzma2 -mx=5 -mfb=32 -md=16m -ms=on -mf=BCJ2 -v2140000000b ComfyUI_Windows_portable_cu124.7z ComfyUI_Windows_portable
+"C:\Program Files\7-Zip\7z.exe" a -t7z -m0=lzma2 -mx=3 -mfb=32 -md=4m -ms=on -mf=BCJ2 -v2140000000b ComfyUI_Windows_portable_cu124.7z ComfyUI_Windows_portable
 
 ls -lahF
