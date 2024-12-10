1f574b4489b1515901a2ae1a4c0e4a5534b2063e
YAN Wenkun <code@yanwk.fun>
2024-12-10 09:09:22 +0800

    clean (2/2)
    don't know why git behaves like that

---
+++ builder/attachments/run_nvidia_gpu.bat
@@ -0,0 +1,37 @@
+@REM To set proxy, edit and uncomment the two lines below (remove 'rem ' in the beginning of line).
+rem set HTTP_PROXY=http://localhost:1081
+rem set HTTPS_PROXY=http://localhost:1081
+
+@REM To set mirror site for PIP & HuggingFace Hub, uncomment and edit the two lines below.
+rem set PIP_INDEX_URL=https://mirrors.tuna.tsinghua.edu.cn/pypi/web/simple
+rem set HF_ENDPOINT=https://hf-mirror.com
+
+@REM To set HuggingFace Access Token, uncomment and edit the line below.
+@REM https://huggingface.co/settings/tokens
+rem set HF_TOKEN=
+
+@REM To enable HuggingFace Hub's experimental high-speed file transfer, uncomment the line below.
+@REM https://huggingface.co/docs/huggingface_hub/hf_transfer
+rem set HF_HUB_ENABLE_HF_TRANSFER=1
+
+@REM This command redirects HuggingFace-Hub to download model files in this folder.
+set HF_HUB_CACHE=%~dp0\HuggingFaceHub
+
+@REM This command will set PATH environment variable.
+set PATH=%PATH%;%~dp0\python_embeded\Scripts
+
+@REM This command will let the .pyc files to be stored in one place.
+set PYTHONPYCACHEPREFIX=%~dp0\pycache
+
+@REM This command will copy u2net.onnx to user's home path, to skip download at first start.
+IF NOT EXIST "%USERPROFILE%\.u2net\u2net.onnx" (
+    IF EXIST ".\extras\u2net.onnx" (
+        mkdir "%USERPROFILE%\.u2net" 2>nul
+        copy ".\extras\u2net.onnx" "%USERPROFILE%\.u2net\u2net.onnx"
+    )
+)
+
+@REM If you don't want the browser to open automatically, add " --disable-auto-launch" (without quotation marks) to the end of the line below.
+.\python_embeded\python.exe -s ComfyUI\main.py --windows-standalone-build
+
+pause
