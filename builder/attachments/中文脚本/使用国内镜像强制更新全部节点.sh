3f1e9d71062256f435a9deed2c0ab7984dde08af
YAN Wenkun <code@yanwk.fun>
2025-01-02 23:12:40 +0800

    Update URL for gh-proxy

--- builder/attachments/中文脚本/使用国内镜像强制更新全部节点.sh
+++ builder/attachments/中文脚本/使用国内镜像强制更新全部节点.sh
@@ -4,10 +4,10 @@
 set -eu
 
 # 如果希望“尝试更新但不强制”，删除 git reset 行以避免还原本地变更
-function change_url_or_pull () {
+function set_url_and_pull () {
     git_remote_url=$(git -C "$1" remote get-url origin) ;
 
-    if [[ $git_remote_url =~ ^(https:\/\/ghp\.ci\/)(.*)(\.git)$ ]]; then
+    if [[ $git_remote_url =~ ^(https:\/\/gh-proxy\.com\/)(.*)(\.git)$ ]]; then
         echo "正在更新: $1" ;
         git -C "$1" reset --hard ;
         git -C "$1" pull --ff-only ;
@@ -18,16 +18,23 @@ function change_url_or_pull () {
         git -C "$1" remote set-url origin "https://gh-proxy.com/$git_remote_url" ;
         git -C "$1" pull --ff-only ;
         echo "更新完成: $1" ;
+    elif [[ $git_remote_url =~ ^(https:\/\/ghp\.ci\/)((https:\/\/github\.com\/)(.*)(\.git))$ ]]; then
+        echo "正在修改URL并更新: $1" ;
+        echo "提取URL: ${BASH_REMATCH[2]}" ;
+        git -C "$1" reset --hard ;
+        git -C "$1" remote set-url origin "https://gh-proxy.com/${BASH_REMATCH[2]}" ;
+        git -C "$1" pull --ff-only ;
+        echo "更新完成: $1" ;
     fi ;
 }
 
-change_url_or_pull ComfyUI
+set_url_and_pull ComfyUI
 
-# 这里使用 & 将任务置入后台，以实现多线程，并等待全部任务完成
+# 这里使用 & 将任务置入后台，以实现多线程（多进程），并等待全部任务完成
 cd ./ComfyUI/custom_nodes
 for D in *; do
     if [ -d "${D}" ]; then
-        change_url_or_pull "${D}" &
+        set_url_and_pull "${D}" &
     fi
 done
 
