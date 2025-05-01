2f60c2389486da0d209ffe4b7b97d516af159542
YAN Wenkun <code@yanwk.fun>
2025-05-01 22:27:21 +0800

    Upgrade to CUDA 12.8

--- builder/stage3.sh
+++ builder/stage3.sh
@@ -14,10 +14,10 @@ mkdir -p m_folder/ComfyUI_Windows_portable/ComfyUI
 mv "ComfyUI_Windows_portable/ComfyUI/models"  "m_folder/ComfyUI_Windows_portable/ComfyUI/models"
 git -C "ComfyUI_Windows_portable/ComfyUI" checkout "models"
 
-"C:\Program Files\7-Zip\7z.exe" a -t7z -m0=lzma2 -mx=7 -mfb=64 -md=128m -ms=on -mf=BCJ2 -v2140000000b ComfyUI_Windows_portable_cu126.7z ComfyUI_Windows_portable
+"C:\Program Files\7-Zip\7z.exe" a -t7z -m0=lzma2 -mx=7 -mfb=64 -md=128m -ms=on -mf=BCJ2 -v2140000000b ComfyUI_Windows_portable_cu128.7z ComfyUI_Windows_portable
 
 # In case you need faster compression, comment the line above, and uncomment the line below. 
-# "C:\Program Files\7-Zip\7z.exe" a -tzip -v2140000000b ComfyUI_Windows_portable_cu126.zip ComfyUI_Windows_portable
+# "C:\Program Files\7-Zip\7z.exe" a -tzip -v2140000000b ComfyUI_Windows_portable_cu128.zip ComfyUI_Windows_portable
 
 cd m_folder
 "C:\Program Files\7-Zip\7z.exe" a -tzip -v2140000000b models.zip ComfyUI_Windows_portable
