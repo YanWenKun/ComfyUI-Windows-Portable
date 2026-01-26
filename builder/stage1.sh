14d578706a74bf205f797ff5f163f099c6cfa271
YAN Wenkun <code@yanwk.fun>
2026-01-26 10:30:41 +0800

    Remove manager_requirements Overrides custom-node-based Manager

--- builder/stage1.sh
+++ builder/stage1.sh
@@ -43,7 +43,6 @@ $pip_exe install -r "$workdir"/pak8.txt
 # Install comfyui-frontend-package, version determined by ComfyUI release version.
 latest_tag=$(curl -sL https://api.github.com/repos/Comfy-Org/ComfyUI/tags | jq -r '.[0].name')
 $pip_exe install -r "https://github.com/Comfy-Org/ComfyUI/raw/refs/tags/${latest_tag}/requirements.txt"
-$pip_exe install -r "https://github.com/Comfy-Org/ComfyUI/raw/refs/tags/${latest_tag}/manager_requirements.txt"
 
 $pip_exe install -r "$workdir"/pakX.txt
 $pip_exe install -r "$workdir"/pakY.txt
